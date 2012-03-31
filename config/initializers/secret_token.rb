0cc6b8da4bbd7b75665c93645ff1ff4cf7a8a4d2
Simon Chiu <skhchiu@gmail.com>
2012-03-31 09:10:09 -0400

    first commit, added gems for twilio and twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CallManager::Application.config.secret_token = '5df90048243ed0d29392625321c2ecc2b5c64c76feaf45cc1d4606e3633937cfe4e5bcfdcd5f31dc5e74d7024950f8380693c24b7718263d361be49a6f36259b'
