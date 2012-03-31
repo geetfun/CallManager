96d8fd9b6cf5b0b53ad5265c6180aa1f0356d58a
Simon Chiu <skhchiu@gmail.com>
2012-03-31 15:43:48 -0400

    Token Controller will create and respond in json

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -3,14 +3,11 @@ class TokensController < ApplicationController
   respond_to :json
   
   def create
-    @stock = current_user.stocks.new(params[:stock])
-    if @stock.save
-      flash[:notice] = "Stock added"
-    end
-    respond_with(@stock)
-    
     @token = Token.new(params[:token])
-    @token.generate
+    if @tocken.save
+      flash[:notice] = "Token generated"
+    end
+    respond_with(@token)
   end
   
 end
